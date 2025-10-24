a042bc1a6898754f70ac224a8b6fb46fb72f1fd4
fallenfall0357-test-repository <fallenfall0357@gmail.com>
2025-10-24 15:31:33 +0900

    f

--- diffusion_model/data/AUDIOCAPS/down_ds.py
+++ diffusion_model/data/AUDIOCAPS/down_ds.py
@@ -31,22 +31,26 @@ def save_mel_item(split, idx, mel):
     torch.save(mel.cpu(), save_path)
 
 # ---------------- Process one dataset ----------------
-def process_split(split_name):
+def process_split(split_name,limit=100):
     print(f"🔹 Processing split: {split_name}")
-    ds = load_dataset("jp1924/AudioCaps", split=split_name, cache_dir="./cache")
+    ds = load_dataset("jp1924/AudioCaps", split=split_name, cache_dir="./cache", streaming=True,features=None)
 
     save_dir = os.path.join(SAVE_ROOT, split_name)
     os.makedirs(save_dir, exist_ok=True)
 
     for i, item in enumerate(tqdm(ds, desc=f"{split_name}")):
+        if i >= limit:
+            break
         save_path = os.path.join(save_dir, f"{i:06d}.pt")
         if os.path.exists(save_path):
             continue  # skip processed
 
         try:
-            audio = item["audio"]
-            waveform = torch.tensor(audio["array"]).float()
-            sr = audio["sampling_rate"]
+            audio_path = item["audio"]["path"]
+            if not os.path.exists(audio_path):
+                waveform, sr = torchaudio.load(audio_path)
+            else:
+                waveform, sr = torchaudio.load(audio_path)
 
             # 重采样
             if sr != SAMPLE_RATE:
@@ -68,6 +72,7 @@ def process_split(split_name):
 
 
 if __name__ == "__main__":
-    for split in ["train", "validation", "test"]:
-        process_split(split)
+    process_split("train",limit=50)
+    process_split("validation",limit=10)
+    process_split("test",limit=10)
 
